7f18522d31eeeaa4fa67bde654e869716a845b3d
Mohammed Zakaria Alalaya <mohammed.alalaya@gmail.com>
2021-06-22 02:17:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Write your code here!
 const newHeader = document.querySelector('#victory');
 
+<<<<<<< HEAD
 newHeader.innerHTML = `Mohammed is the champion`
+=======
+newHeader.innerHTML = `Mohammed`
+>>>>>>> da25cccd73fc6b89169415decc8192b6e2daff93
